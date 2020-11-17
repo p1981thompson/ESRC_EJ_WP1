d8d90a457ad7432293f50934336cffb102f2738e
p1981thompson <paul.thompson@psy.ox.ac.uk>
2020-11-17 14:18:17 +0000

    added some better commenting on the copula script. Also added the LPA model bit.

--- simulations/LPA_copula_sim.R
+++ simulations/LPA_copula_sim.R
@@ -1,36 +1,53 @@
+#==============================================================================================#
+# Alternate data simulation for LPA model using copula    
+#==============================================================================================#
 
+#17-11-2020
 
+#load R packages
 library(copula)
 library(KScorrect)
 
 
-#=========================================================================#
+#==============================================================================================#
 
-#Define copula
-#=========================================================================#
+#Define copula: links the marginal distributions together to form the joint distribution
+#==============================================================================================#
 n_ppts <- 1000
 
 set.seed(100)
-# constructs an elliptical copula (this has inputs of the correlation matrix - overall, pooled estimates not classones.)
-myCop <- normalCopula(param=c(0.5,0.4,0.4,0.6,
+# constructs a copula (this has inputs of the correlation matrix - overall, pooled estimates not class ones.) We use a normal or gaussian copula here but there are others. I just seeing whether we might get better results using a different copula. 
+myCop <- normalCopula(param=c(0.5,0.4,0.4,0.6, # param is the correlation matrix between the marginal variables
                                   0.6,0.5,0.2,
                                       0.7,0.2,
                                           0.2), 
-                      dim=5, dispstr="un")
+                      dim=5, dispstr="un") #dim is number of marginal variables; dispstr is the structure of the correlation matrix, so in our case we have unstructured as the variables are correlated but not structured unlike if we had repeated measures.
+#==============================================================================================#
 
-# creates a multivariate distribution via copula (each line below is the marginal distribution for each variable, with means and sds for each profile)
-myMvd <- mvdc(copula=myCop, margins=c("mixnorm", "mixnorm", "mixnorm", "mixnorm", "mixnorm"),
-              paramMargins=list(list(mean=c(105,90,80),  sd=c(5,7,10),pro=c(0.6,0.2,0.2)), 
-                                list(mean=c(105,90,80),  sd=c(5,7,10),pro=c(0.6,0.2,0.2)), 
-                                list(mean=c(105,90,80),  sd=c(5,7,10),pro=c(0.6,0.2,0.2)), 
-                                list(mean=c(105,90,80),  sd=c(5,7,10),pro=c(0.6,0.2,0.2)), 
-                                list(mean=c(105,90,80),  sd=c(5,7,10),pro=c(0.6,0.2,0.2))))
+# creates a multivariate distribution using the copula (link) - each line below is the marginal distribution for each variable, with means and sds for each profile.
 
-#Generate data based on the copula.
-z2 <- rMvdc(n_ppts, myMvd)
-colnames(z2) <- c("nara_comp", "nara_acc", "word_acc", "nonword_acc", "wold_comp")
+# We define each individual variable's distribution within the multivariate distribution. So each variable has its own distribution, in our case we want a mixture normal distribution; see here: https://en.wikipedia.org/wiki/Multimodal_distribution#Mixture_of_two_normal_distributions
 
+# The mvdc function sets up the multivariate distribution, specifying the copula (link), then the marginals and finally the parameters for setting the location (mean) and spread (sd) and mixing proportions of the marginal distributions.
+
+myMvd <- mvdc(copula=myCop, margins=c("mixnorm", "mixnorm", "mixnorm", "mixnorm", "mixnorm"), #These are the mixture distributions for each variable.
+              paramMargins=list(list(mean=c(105,90,80),  sd=c(5,7,10),pro=c(0.6,0.2,0.2)), # NARA - comprehension
+                                list(mean=c(105,90,80),  sd=c(5,7,10),pro=c(0.6,0.2,0.2)), # NARA - accuracy
+                                list(mean=c(105,90,80),  sd=c(5,7,10),pro=c(0.6,0.2,0.2)), # Word reading
+                                list(mean=c(105,90,80),  sd=c(5,7,10),pro=c(0.6,0.2,0.2)), # Nonword reading
+                                list(mean=c(105,90,80),  sd=c(5,7,10),pro=c(0.6,0.2,0.2)))) # WOLD listening
+
+#Generate data based on the multivariate distribution. Same as if you did rnorm(), you are saying that you want to take X number of samples from that distribution.
+simdat2 <- rMvdc(n_ppts, myMvd)
+colnames(simdat2) <- c("nara_comp", "nara_acc", "word_acc", "nonword_acc", "wold_comp")
+
+#==============================================================================================#
 #plot to see if we get something sensible.
-pairs.panels(z2)
+pairs.panels(simdat2)
 
+#==============================================================================================#
+#We can run a basic version of your script below (no missing data). It still does not pick up the structure, so I'm thinking on that. 
 
+sim_dat2 %>% 
+  estimate_profiles(1:10) %>% 
+  compare_solutions(statistics = c("AIC", "BIC", "BLRT_val", "BLRT_p"))
